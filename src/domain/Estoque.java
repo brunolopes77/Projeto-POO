130890c1eff1d91ac8e2fb0633e50a6eb33a58e2
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-03-27 10:47:52 -0300

    test remove

--- src/domain/Estoque.java
+++ src/domain/Estoque.java
@@ -1,7 +1,9 @@
 package domain;
 
-public class Estoque {
+import java.util.Scanner;
 
+public class Estoque {
+    Scanner scanner = new Scanner(System.in);
     String[][] arrayProduto = new String[4][4];
     int count = 1;
     int aux = 1;
@@ -27,17 +29,21 @@ public class Estoque {
     }
 
     public void removeProduto(){
+        int i;
         System.out.println("Qual produto deseja remover?");
-        for (int i = 1; i < 4; i++) {
+        for (i = 1; i < 4; i++) {
             System.out.println(i+". "+arrayProduto[i][1]);
         }
+        System.out.print("(coloque a posição dele): ");
+        i = scanner.nextInt();
+        arrayProduto[i][1] = "-------";
     }
 
     public void imprimeProdutos() {
         for (int i = 1; i < 4; i++) {
-            for (int j = 1; j < 4; j++) {
-                System.out.println(arrayProduto[i][j]);
-            }
+           // for (int j = 1; j < 4; j++) {
+                System.out.println(arrayProduto[i][1]);
+
             System.out.println();
         }
     }
--- src/test/Main.java
+++ src/test/Main.java
@@ -26,9 +26,9 @@ public class Main {
                     case 2:
                         estoque.removeProduto();
                 }
-                break;
+
             }
         }
-        //estoque.imprimeProdutos();
+        estoque.imprimeProdutos();
     }
 }
