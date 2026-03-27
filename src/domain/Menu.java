1d27b350d133c0bde172911338870de3e514dcaa
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-03-27 16:10:52 -0300

    commit

--- src/domain/Menu.java
+++ src/domain/Menu.java
@@ -5,6 +5,7 @@ public class Menu {
         System.out.println("DESEJA FAZER:");
         System.out.println("[1] - Adicionar produto ao estoque.");
         System.out.println("[2] - Remover produto do estoque.");
+        System.out.println("[3] - Listar estoque.");
         System.out.print("Opção:");
 
     }
