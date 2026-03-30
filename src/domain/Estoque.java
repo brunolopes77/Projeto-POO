59cd387c0881d63eb7f09b8eb37f8aecf9576916
Bruno Lopes Borges <brunolopesborges2007@gmail.com>
2026-03-30 11:09:15 -0300

    refatoraçao

--- src/domain/Estoque.java
+++ src/domain/Estoque.java
@@ -4,43 +4,45 @@ import java.util.Scanner;
 
 public class Estoque {
     Scanner scanner = new Scanner(System.in);
-    String[][] arrayProduto = new String[4][4];
-    int count = 1;
-    int aux = 1;
+    String[][] arrayProduto = new String[3][3];
+    int count = 0;
+    int aux = 0;
     String nome;
     float preco;
     int quantidade;
 
     public void addProduto(Produto produto) {
         nome = produto.getNome();
-        arrayProduto[aux][1] = nome;
+        arrayProduto[aux][0] = nome;
 
         preco = produto.getPreco();
-        arrayProduto[aux][2] = String.valueOf(preco);
+        arrayProduto[aux][1] = String.valueOf(preco);
 
         quantidade = produto.getQuantidade();
-        arrayProduto[aux][3] = String.valueOf(quantidade);
+        arrayProduto[aux][2] = String.valueOf(quantidade);
 
         aux++;
     }
 
 
     public void removeProduto() {
-        int i;
+
         System.out.println("Qual produto deseja remover?");
-        for (i = 1; i < 4; i++) {
-            System.out.println(i + ". " + arrayProduto[i][1]);
+        int count2 = 1;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(count2 + ". " + arrayProduto[i][0]);
+            count2++;
         }
         System.out.print("(coloque a posição dele): ");
-        i = scanner.nextInt();
-        for (int j = 1; j < 4; j++) {
+        int i =  (scanner.nextInt() - 1);
+        for (int j = 0; j < 3; j++) {
             arrayProduto[i][j] = "-------";
         }
     }
 
     public void imprimeProdutos() {
-        for (int i = 1; i < 4; i++) {
-            for (int j = 1; j < 4; j++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.println(arrayProduto[i][j]);
             }
             System.out.println();
